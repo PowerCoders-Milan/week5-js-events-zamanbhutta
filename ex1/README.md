7546afc0b70f4bee61ba61b415e02dec3434ffa0
zamanbhutta <marham.ayat@gmail.com>
2021-03-04 14:24:38 +0100

    done 1

--- ex1/README.md
+++ ex1/README.md
@@ -5,4 +5,3 @@ Write a JavaScript program to change the color of the bold words when the user h
 ## EXTRA (to do when everything is finished)
 
 Make the color random for each bold word when the user hovers.
-
--- ex1/exercise.js
+++ ex1/exercise.js
@@ -1,3 +1,35 @@
-// define your functions here
 
-// call the functions and connect them to an event
+var strong = document.querySelectorAll("strong");
+var a = document.querySelector("a");
+
+function changeColor(color){
+    strong.forEach(function(item){
+        item.style.color = color;
+    });
+}
+a.addEventListener('mouseenter', function() {changeColor('red')});
+a.addEventListener('mouseleave', function() {changeColor('black')});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
